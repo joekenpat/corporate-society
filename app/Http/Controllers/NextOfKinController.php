9b86c6096928f75d0edd6f3f150a9c5dd856d44f
Patrick Joel <joekenpat@gmail.com>
2021-04-29 21:42:45 +0100

    first commit

---
+++ app/Http/Controllers/NextOfKinController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\NextOfKin;
+use Illuminate\Http\Request;
+
+class NextOfKinController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\NextOfKin  $nextOfKin
+     * @return \Illuminate\Http\Response
+     */
+    public function show(NextOfKin $nextOfKin)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\NextOfKin  $nextOfKin
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, NextOfKin $nextOfKin)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\NextOfKin  $nextOfKin
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(NextOfKin $nextOfKin)
+    {
+        //
+    }
+}
---
+++ app/Models/User.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'firstName',
+        'lastName',
+        'middleName',
+        'gender',
+        'phone',
+        'maritalStatus',
+        'disability',
+        'dob',
+        'address1',
+        'address2',
+        'state_id',
+        'lga_id',
+        'town_id',
+        'employmentStatus',
+        'identificationType',
+        'profileImage',
+        'identificationImage',
+        'email',
+        'password',
+        'status',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
