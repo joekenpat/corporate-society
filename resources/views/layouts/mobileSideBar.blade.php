ebf0f4c2249f0e6d6f6e88af320ddf67d016d87f
Patrick Joel <joekenpat@gmail.com>
2021-05-15 14:30:17 +0100

    added profile picture to the mobileside bar

--- resources/views/layouts/mobileSideBar.blade.php
+++ resources/views/layouts/mobileSideBar.blade.php
@@ -4,6 +4,12 @@
     -->
 <div id="mySidenav" class="sidenav">
   <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#" class="mb-0 py-0 text-center">
+    <img
+      src="{{auth()->user()->profile_image!=(null||"")?asset('images/profile/'.auth()->user()->profile_image):asset('images/misc/default_avatar.png')}}"
+      style="height: 60px; width: 60px; border-radius: 50px;"></a>
+  <a href="#" class="mb-0 pb-0 text-center" style="text-decoration: none;color:#fff">{{ auth()->user()->full_name }}</a>
+  <a href="#" class="mb-0 pt-0 pb-3 text-center" style="text-decoration: none;color:#fff">{{auth()->user()->email}}</a>
   <a href="{{route('dashboard')}}">Dashboard</a>
   <a href="{{route('deposit_create')}}">Deposit</a>
   <a href="{{route('withdrawal_create')}}">Withdraw</a>
