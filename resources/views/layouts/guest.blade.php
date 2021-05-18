d0ff342eeb25ab417ba899ee6f2a12b04f3af253
Patrick Joel <joekenpat@gmail.com>
2021-05-18 05:38:35 +0100

    added meta desc

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description"
+    content="Is an organization that is born out of passion and needs to impact positively on the economic growth of the nation, to reduce the rate of poverty, and to improve the general wellbeing of the people.">
   <link rel="apple-touch-icon" sizes="180x180" href="{{asset("apple-touch-icon.png")}}">
   <link rel="icon" type="image/png" sizes="32x32" href="{{asset("images/misc/favicon-32x32.png")}}">
   <link rel="icon" type="image/png" sizes="16x16" href="{{asset("images/misc/favicon-16x16.png")}}">
